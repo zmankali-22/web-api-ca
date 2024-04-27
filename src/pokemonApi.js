f0fe1b51ee44508c6b4fcbd4ad69e13113062025
acharya <sav.acharya22@gmail.com>
2024-04-27 10:00:29 +1000

    feat: types of names display

--- src/pokemonApi.js
+++ src/pokemonApi.js
@@ -11,6 +11,17 @@ async function getPokemonData() {
 
 function putDataOnPage(dataToDisplay) {
     document.getElementsByClassName("pokemonName")[0].textContent = dataToDisplay.name
+
+    let type1Display = document.getElementsByClassName("pokemonType1")[0];
+    let type2Display = document.getElementsByClassName("pokemonType2")[0];
+
+    type1Display.textContent = "Type 1:" + dataToDisplay.types[0].type.name;
+
+    if(dataToDisplay.types[1]) {
+        type2Display.textContent = "Type 2:" + dataToDisplay.types[1].type.name;
+    } else {
+        type2Display.textContent = "Type 2:";
+    }
 }
 
 // Button calls this
